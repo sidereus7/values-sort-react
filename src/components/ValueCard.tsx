1b596904b185ee98d12e3daa506c942aea453f89
Sara Jackson <sidereus7@gmail.com>
2026-03-12 13:00:36 -0700

    Make ValueCard droppable with visual transform

--- src/components/ValueCard.tsx
+++ src/components/ValueCard.tsx
@@ -1,5 +1,8 @@
 import type { Value } from '../types';
 
+import { useDraggable } from '@dnd-kit/core';
+import { CSS } from '@dnd-kit/utilities';
+
 import styles from './ValueCard.module.css';
 
 interface ValueCardProps {
@@ -7,8 +10,16 @@ interface ValueCardProps {
 }
 
 export default function ValueCard({ value }: ValueCardProps) {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: value.id,
+  });
+
+  const style = {
+    transform: CSS.Translate.toString(transform),
+  };
+  
   return (
-    <div className={styles.card}>
+    <div ref={setNodeRef} {...listeners} {...attributes} style={style} className={styles.card}>
       <p className={styles.title}>{value.title}</p>
       <p className={styles.description}>{value.description}</p>
     </div>
