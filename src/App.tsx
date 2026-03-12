a801afd0bbaf4291cf10b7a060098f57edfa7f6a
Sara Jackson <sidereus7@gmail.com>
2026-03-12 12:41:44 -0700

    Add dnd-kit and basic handleDragEnd behavior for cards/columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 
+import { DndContext, type DragEndEvent } from '@dnd-kit/core';
+
 import CardDeck from './components/CardDeck';
 import ValueColumn from './components/ValueColumn';
 
@@ -18,29 +20,44 @@ const initialValues: Value[] = valuesData.map((value, index) => ({
 export default function App() {
   const [values, setValues] = useState<Value[]>(initialValues);
 
+  // give dragged card the importance of the destination column
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event;
+
+    if (!over) { return; }
+    
+    setValues(values.map((value) => 
+      value.id === active.id 
+        ? { ...value, importance: over.id as Importance }
+        : value
+    ));
+  }
+
   return (
-    <div className={styles.app}>
-      <p className={styles.title}>Values Sort</p>
+    <DndContext onDragEnd={handleDragEnd}>
+      <div className={styles.app}>
+        <p className={styles.title}>Values Sort</p>
 
-      <CardDeck values={values.filter((value) => value.importance === 'unsorted')}/>
+        <CardDeck values={values.filter((value) => value.importance === 'unsorted')}/>
 
-      <div className={styles.columns}>
-        <ValueColumn
-          header='Very Important'
-          importance='very-important'
-          values={values.filter((value) => value.importance === 'very-important')}
-        />
-        <ValueColumn
-          header='Important'
-          importance='important'
-          values={values.filter((value) => value.importance === 'important')}
-        />
-        <ValueColumn
-          header='Not Important'
-          importance='not-important'
-          values={values.filter((value) => value.importance === 'not-important')}
-        />
+        <div className={styles.columns}>
+          <ValueColumn
+            header='Very Important'
+            importance='very-important'
+            values={values.filter((value) => value.importance === 'very-important')}
+          />
+          <ValueColumn
+            header='Important'
+            importance='important'
+            values={values.filter((value) => value.importance === 'important')}
+          />
+          <ValueColumn
+            header='Not Important'
+            importance='not-important'
+            values={values.filter((value) => value.importance === 'not-important')}
+          />
+        </div>
       </div>
-    </div>
+    </DndContext>
   );
 }
